0c2d0df047069060dedc6bb0e21b5e1e40349c8b
hritik-dubey <dubeyhritik32@gmail.com>
2022-05-04 21:56:57 +0530

    complete with code without the validation

--- src/controllers/collegecontrollers.js
+++ src/controllers/collegecontrollers.js
@@ -1,9 +1,3 @@
-// POST /functionup/colleges
-// Create a college - a document for each member of the group
-
-// The logo link will be provided to you by the mentors. This link is a s3 (Amazon's Simple Service) url. Try accessing the link to see if the link is public or not.
-
-// Endpoint: BASE_URL/functionup/colleges
 const collegemodel = require("../models/collegemodel");
 const internmodel = require("../models/internmodel");
 const createCollege = async function (req, res) {
@@ -16,25 +10,21 @@ const createCollege = async function (req, res) {
     }
 }
 
-
-
-// GET /functionup/collegeDetails
-// Returns the college details for the requested college (Expect a query parameter by the name collegeName. This is anabbreviated college name. For example iith)
-// Returns the list of all interns who have applied for internship at this college.
-// The response structure should look like this
 const collegeDetails = async function (req, res) {
     try {
         let collegeName = req.query.name;
-
+        let emptyobj = {}
         if (collegeName) {
-            let data = await collegemodel.find({ name: collegeName })
+            let collegeinfo = await collegemodel.findOne({ name: collegeName })
+            emptyobj.name = collegeinfo.name;
+            emptyobj.fullName = collegeinfo.fullName;
+            emptyobj.logoLink = collegeinfo.logoLink;
             let collegeid = await collegemodel.find({ name: collegeName }).select({ id: 1 })
-            if (datawithid) {
+            if (collegeid) {
                 let interns = await internmodel.find({ collegeId: collegeid })
-                return res.status(200).send({ status: true, msg: "interne details", data: interns })
+                emptyobj.interests = interns
+                return res.status(200).send({ status: true, msg: "intern details", data: emptyobj })
             }
-
-            return res.status(200).send({ status: true, msg: "college details", data: data })
         }
     } catch (error) {
         res.status(500).send({ status: false, error: error.message })
